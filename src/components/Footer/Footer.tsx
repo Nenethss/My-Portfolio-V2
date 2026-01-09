74d028b11bcee00bd0b884daaa7a8fe96c4ab214
Kenneth San Pedro <kennethsanpedro1108@gmail.com>
2026-01-09 08:39:09 +0800

    Change the year on footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className="bg-blue-500 mx-auto px-6 py-4 justify-items-center">
       <p className="text-center text-white  p-16 text-[12px]">
-        © 2025 KENNETH SAN PEDRO. ALL RIGHTS RESERVED
+        © 2026 KENNETH SAN PEDRO. ALL RIGHTS RESERVED
       </p>
     </div>
   );
